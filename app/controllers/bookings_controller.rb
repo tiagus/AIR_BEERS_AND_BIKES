557ca01469b6b5e00339e61ae609671f3c055864
marco varela <marco.varela12@gmail.com>
2020-02-24 16:33:26 +0000

    Added Models, Routes and Controllers with respective views

---
+++ app/controllers/bookings_controller.rb
@@ -0,0 +1,47 @@
+class BookingsController < ApplicationController
+  before_action :find_booking, only: [:show, :update, :edit, :destroy]
+
+  def index
+    @bookings = Booking.all
+  end
+
+  def show
+  end
+
+
+  def new
+    @booking = Booking.new
+  end
+
+  def create
+    @booking = Booking.new(booking_params)
+    if @booking.save!
+      redirect_to @booking
+    else
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    @booking.update(booking_params)
+    redirect_to @booking
+  end
+
+  def destroy
+    @booking.destroy
+    redirect_to root_path
+  end
+
+  private
+
+  def find_booking
+    @booking = Booking.find(params[:id])
+  end
+
+  def booking_params
+    params.require(:booking).permit(:start_date, :end_date)
+  end
+end
