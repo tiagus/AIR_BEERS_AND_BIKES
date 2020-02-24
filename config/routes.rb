21b82e24245891b412fe21ec6bcc5f5f941cac80
Stefano Singer <singerstefano@gmail.com>
2020-02-24 17:47:57 +0000

    index and show erb

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,11 @@
 Rails.application.routes.draw do
-  get 'bikes/index'
-  get 'bikes/show'
-  get 'bookings/show'
-  get 'bookings/index'
-  get 'bookings/new'
-  get 'bookings/edit'
+
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :bookings do
-    resources :bikes, only: [:index, :show]
+    #resources :bikes, only: [:index, :show]
   end
+
+  resources :bikes, only: [:index, :show]
 end
