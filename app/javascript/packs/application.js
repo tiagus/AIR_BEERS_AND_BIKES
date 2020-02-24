ebb8904c41b16b987527f2e090008078de3b63dd
ricardo silva <rcorreiadasilva@outlook.com>
2020-02-24 16:55:30 +0000

    navbar + footer + home page creation

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,1 +1,21 @@
 import "bootstrap";
+// import { sliderSiema } from 'slider';
+// sliderSiema();
+// import Siema from 'siema';
+//  const mySiema = new Siema({
+//   selector: '.siema',
+//   duration: 200,
+//   easing: 'ease-out',
+//   perPage: 1,
+//   startIndex: 0,
+//   draggable: true,
+//   multipleDrag: true,
+//   threshold: 20,
+//   loop: tr,
+//   rtl: false,
+//   onInit: () => {},
+//   onChange: () => {},
+// });
+// document.querySelector('.prev').addEventListener('click', () => mySiema.prev());
+// document.querySelector('.next').addEventListener('click', () => mySiema.next());
+// console.log("hello")
