f28d58683c506593dff4059b296c85aec1c971b8
Tiago Santos <tiago@convertodigital.com>
2020-02-25 07:53:02 +0000

    fixing problem with routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,10 @@
 Rails.application.routes.draw do
-  get 'bikes/index'
-  get 'bikes/show'
-  get 'bookings/show'
-  get 'bookings/index'
-  get 'bookings/new'
-  get 'bookings/edit'
+
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :bookings do
-    resources :bikes, only: [:index, :show]
+  resources :bikes, only: [:index, :show] do
+    resources :bookings
   end
+
 end
